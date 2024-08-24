9009fe66d14c06ad540145741e18a64399e32cb0
Bennoama <120720815+Bennoama@users.noreply.github.com>
2024-08-24 15:27:09 +0300

    Improved app bar

--- src/framework/appBar.tsx
+++ src/framework/appBar.tsx
@@ -5,29 +5,78 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import { styled } from '@mui/material/styles';
 
+// Define styles for the dialog's Paper component using styled
+const StyledDialog = styled(Dialog)(() => ({
+  '& .MuiPaper-root': {
+    position: 'sticky',
+    top: '80%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    marginTop: '-25%',
+    border: '3px solid black',
+    backgroundColor: 'lightgray',
+  },
+}));
+
+const textStyling: React.CSSProperties = {
+  width: 'auto',
+  position: 'relative',
+  display: 'block',
+  textAlign: 'justify',
+  margin: '10px',
+  justifyContent: 'center',
+  alignItems: 'stretch'
+};
+
+const headerStyling: React.CSSProperties = {
+  ...textStyling,
+  display: 'flex'
+};
 
 export const SpendSentinelAppBar: FunctionComponent = () => {
+  const [infoBoxVisible, setInfoBoxVisible] = useState(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           Spend Sentinel App
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          </Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
           <IconButton
-            onClick={()=>{alert("This does nothing!! Nothing I tell you!");}}
-            size="large"
-            edge="start"
+            onClick={() => setInfoBoxVisible(true)}
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            >
+          >
             <InfoIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
+      <StyledDialog
+        onClose={() => setInfoBoxVisible(false)}
+        open={infoBoxVisible}
+      >
+        <h1 style={headerStyling}>About Spend-Sentinel</h1>
+        <ul style={textStyling}>
+          {getInfoBulletPoints().map((bulletPoint:string) => {
+            return (<li key={bulletPoint}>{bulletPoint}</li>);
+          })}
+        </ul>
+      </StyledDialog>
     </Box>
   );
+};
+
+
+const getInfoBulletPoints = (): string[] => {
+  const bulletPointsArray = [
+    "Spend-Sentinel is an open-source application designed to help users track their money transactions made through credit cards.",
+    "It provides an intuitive way to monitor and categorize expenses, enabling users to gain a clearer understanding of their spending habits.",
+    "Whether you're managing personal finances or keeping an eye on business expenditures, Spend-Sentinel offers a reliable solution to stay on top of your financial activities.",
+    "Thank you for using Spend-Sentinel, and we hope you have a great experience!",
+    "If you encounter any issues or have feedback, please don't hesitate to contact us.",
+  ];
+  return bulletPointsArray;
 }
